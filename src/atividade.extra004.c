9fd945e54cd72eff3f49987fb4b02783c85b4794
Pedro Henrique <pedrohenriqueqrz16@gmail.com>
2025-09-10 11:09:20 -0300

    Manipulando strings

---
+++ src/atividade.extra004.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+
+int main(){
+    char texto[200];
+    char chave[50];
+    int contador = 0;
+    char *ponteiro;
+
+    printf("Escreva o texto: \n");
+    fgets(texto, sizeof(texto), stdin);
+
+    printf("\nEscreva a palavra chave: \n");
+    fgets(chave, 50, stdin);
+
+    texto[strcspn(texto, "\n")] = '\0';
+    chave[strcspn(chave, "\n")] = '\0';
+
+    ponteiro = texto;
+
+    while ((ponteiro = strstr(ponteiro, chave)) != NULL){
+        contador++;
+        ponteiro += strlen(chave);
+    }
+    
+    if(strstr(texto, chave) != NULL){
+        printf("Palavra encontrada! Ela aparece %i", contador);
+    } else {
+        printf("Palavra não encontrada!");
+    }
+
+    return 0;
+
+}
